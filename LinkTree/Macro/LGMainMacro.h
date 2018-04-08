4c38f4a5da9f17a5aa0fcb8492fa43230ce61f71
lz990377023 <990377023@qq.com>
2018-04-08 19:11:47 +0800

    update main

---
+++ LinkTree/Macro/LGMainMacro.h
@@ -0,0 +1,13 @@
+//
+//  LGMainMacro.h
+//  LinkTree
+//
+//  Created by lizhan on 2018/4/8.
+//  Copyright © 2018年 lizhan. All rights reserved.
+//
+
+#ifndef LGMainMacro_h
+#define LGMainMacro_h
+
+
+#endif /* LGMainMacro_h */
---
+++ LinkTree/Macro/LGUIMacro.h
@@ -0,0 +1,13 @@
+//
+//  LGUIMacro.h
+//  LinkTree
+//
+//  Created by lizhan on 2018/4/8.
+//  Copyright © 2018年 lizhan. All rights reserved.
+//
+
+#ifndef LGUIMacro_h
+#define LGUIMacro_h
+
+
+#endif /* LGUIMacro_h */
